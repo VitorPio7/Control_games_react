4c3b76aa26fda1484c85df5d6d33060434126fe4
MarcosRibeirokr <147077774+MarcosRibeirokr@users.noreply.github.com>
2023-12-10 21:25:37 -0300

    ok

--- Entrega 2/control-games-react/src/configuracao/LocalServer.js
+++
@@ -1,7 +0,0 @@
-// atualizar ip do servidor local
-const ip = '192.168.0.150'
-// atualizar port do servidor, mesma porta do Server.js 
-const port = 3002
-
-
-export const LocalServerUrl = `${ip}:${port}`
